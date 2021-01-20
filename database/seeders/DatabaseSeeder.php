954a07be3d8dbef217ec351dfed7fa617ecb8066
mohamed frouh <mohamedfrouh96@gmail.com>
2021-01-20 16:33:13 +0200

    first commit

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -18,18 +18,6 @@ class DatabaseSeeder extends Seeder
     {
         // \App\Models\User::factory(10)->create();
         $role1=Role::create(['name'=>'Super Admin']);
-        $super=User::create(['name'=>'Super Admin','password'=>bcrypt('12345678'),'email'=>'superadmin@example.com']);
-        $super->assignRole($role1);
-        $role2=Role::create(['name'=>'Admin']);
-        for ($i=1; $i <= 10; $i++) {
-            $admin=User::create(['name'=>"Admin$i",'password'=>bcrypt('12345678'),'email'=>"admin$i@example.com"]);
-            $admin->assignRole($role2);
-        }
-        $role3=Role::create(['name'=>'User']);
-        for ($i=1; $i <= 100; $i++) {
-            $user=User::create(['name'=>"User$i",'password'=>bcrypt('12345678'),'email'=>"user$i@example.com"]);
-            $user->assignRole($role3);
-        }
         $permissions=['admin','role','permission'];
         foreach ($permissions as $key => $permission) {
             $s='s';
@@ -49,6 +37,19 @@ class DatabaseSeeder extends Seeder
         $permission3=['setting','dashboard'];
         foreach ($permission3 as $key => $permission) {
             Permission::create(['name'=>"$permission"]);
+        }
+        $super=User::create(['name'=>'Super Admin','password'=>bcrypt('12345678'),'email'=>'superadmin@example.com']);
+        $super->assignRole($role1);
+        $role2=Role::create(['name'=>'Admin']);
+        $role2->syncPermissions(['setting','dashboard','delete albums','view all users','view all albums']);
+        for ($i=1; $i <= 10; $i++) {
+            $admin=User::create(['name'=>"Admin$i",'password'=>bcrypt('12345678'),'email'=>"admin$i@example.com"]);
+            $admin->assignRole($role2);  
+        }
+        $role3=Role::create(['name'=>'User']);
+        for ($i=1; $i <= 100; $i++) {
+            $user=User::create(['name'=>"User$i",'password'=>bcrypt('12345678'),'email'=>"user$i@example.com"]);
+            $user->assignRole($role3);
         }
     }
 
